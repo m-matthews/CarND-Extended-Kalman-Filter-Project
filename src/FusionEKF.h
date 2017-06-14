af980f8741721a11e0c63dec3b5d191d8a10f5d2
Michael Matthews <m-matthews@users.noreply.github.com>
2017-06-14 20:31:28 +1000

    Implement Kalman Filter and Extended Kalman Filter fusion.

--- src/FusionEKF.h
+++ src/FusionEKF.h
@@ -38,12 +38,15 @@ private:
   // previous timestamp
   long long previous_timestamp_;
 
+  // process and measurement noises
+  int noise_ax_;
+  int noise_ay_;
+
   // tool object used to compute Jacobian and RMSE
   Tools tools;
   Eigen::MatrixXd R_laser_;
   Eigen::MatrixXd R_radar_;
   Eigen::MatrixXd H_laser_;
-  Eigen::MatrixXd Hj_;
 };
 
 #endif /* FusionEKF_H_ */
